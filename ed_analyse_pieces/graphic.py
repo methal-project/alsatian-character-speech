9d579a79d21f56ea7a700a962bb8b980ba8887fd
Qinyue <qinyue.liu@etu.unistra.fr>
2022-06-14 16:09:00 +0200

    group output graph done

--- ed_analyse_pieces/graphic.py
+++ ed_analyse_pieces/graphic.py
@@ -2,6 +2,7 @@ import seaborn as sb
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys, os
+import csv
 
 def plot_emotion(file_paths, emotion_list, filters):
     for i in range(len(file_paths)):
@@ -23,7 +24,42 @@ def plot_emotion(file_paths, emotion_list, filters):
     graph.set_ylabel("emotion level")
     plt.show()
 
-def main():
+def get_moyennes():
+    all_moyen = []
+    piece_moyen = []
+    li_files = os.listdir(".")
+    for name in li_files:
+        if os.path.isdir(name): # Si c'est un repertoire de piece de theatre
+            drama_type = False
+            piece_moyen.append(name)
+            folder_path = name + "/"
+            csv_files = os.listdir(folder_path)
+            for csv_file in csv_files: # csv files dans chaque repertoire de piece
+                if (".csv" in csv_file):
+                    df = pd.read_csv(folder_path + csv_file)
+                    if (drama_type == False):
+                        piece_moyen.append(df["drama_type"].values[0])
+                        drama_type = True
+                    piece_moyen.append(df["avgLexVal"].mean())
+            all_moyen.append(piece_moyen)
+            piece_moyen = []
+    return all_moyen
+
+def write_csv(all_moyen):
+    header = ["shortName", "drama_type", "anger", "anticipation", "arousal", 
+    "disgust", "dominance", "fear", "joy", "sadness", "surprise", "trust", "valence"
+    ]
+    with open("all_pieces_info.csv", "w", encoding="utf-8") as out:
+        writer = csv.writer(out)
+        writer.writerow(header)
+        writer.writerows(all_moyen)
+    
+def group_info():
+    all_moyen = get_moyennes()
+    write_csv(all_moyen)
+    
+
+def single_piece():
     arg_len = len(sys.argv)
     filters = []
     if (arg_len == 3): # si y'a deux args
@@ -43,7 +79,47 @@ def main():
     for filename in emotion_list:
         filepath.append(folder + "/" + filename + ".csv")
     plot_emotion(filepath, emotion_list, filters)
-    
+
+def more_pieces():
+    query = ""
+    df = pd.read_csv("all_pieces_info.csv")
+    if (len(sys.argv) >= 3): # emotion or shortName seulement || emotion + drama_type
+        handle = sys.argv[1]     
+        if (handle == "--emotion"):
+            emotion = sys.argv[2]
+            if (len(sys.argv) == 3):
+                graph = sb.barplot(data = df, x = "shortName", y = emotion, hue="drama_type")
+                graph.set_ylim(0,1)
+                plt.show()
+            else:
+                drama_type = sys.argv[3]
+                query = "drama_type == '" + drama_type + "'"
+                df = df.query(query)
+                graph = sb.barplot(data = df, x = "shortName", y = emotion, hue="drama_type")
+                graph.set_ylim(0,1)
+                plt.show()
+        elif(handle == "--shortName"):
+            shortName = sys.argv[2]
+            query = "shortName == '" + shortName + "'"
+            df = df.query(query)
+            graph = sb.barplot(data = df)
+            graph.set_title(shortName)
+            plt.show()
+    elif (len(sys.argv) == 1): # sans argument, plot tous
+        graph = sb.barplot(data = df)
+        graph.set_ylim(0,1)
+        plt.show()
+    elif(len(sys.argv) == 2): # tous les theatres dans une meme categorie
+        drama_type = sys.argv[1]
+        query = "drama_type == '" + drama_type + "'"
+        df = df.query(query)
+        graph = sb.barplot(data = df)
+        graph.set_ylim(0,1)
+        graph.set_title(drama_type)
+        plt.show()
+
 
 if __name__ == "__main__":
-    main()
+    #single_piece()
+    #df = df.query("shortName == 'am-letzte-maskebal' or shortName == 'arnold-der-pfingstmontag'")
+    more_pieces()
--- summary_stage/semaine8.md
+++ summary_stage/semaine8.md
@@ -3,9 +3,11 @@
 2. fix the bug (pas pouvoir parcourir tous le texte)
 3. seaborn to plot graphs of evaluations of sentiments
 
-#### graphic
+#### graphic chaque piece
 
 lineplot by seaborn
+
+visualization pour chaque piece
 
 ```shell
 python3 graphic.py am-letzte-maskebal anger,anticipation,arousal,disgust,dominance,fear,joy,sadness,surprise,trust,valence speaker
@@ -20,4 +22,36 @@ python3 graphic.py am-letzte-maskebal anger,joy
 ```
 arg 1 et arg 2 sont necessaire, arg 3 optionnel
 
+##### sample output graph:
+![sample photo](../ed_analyse_pieces/am-letzte-maskebal/sample.png)
 
+![sample photo 1](../ed_analyse_pieces/am-letzte-maskebal/sample1.png)
+
+#### graphic groupe
+
+pour chaque emotion de chaque piece, prend une moyenne, et ecrire les resultats statistiques dans un fichier csv.
+
+structure de donnees pour sauvegarder les valeurs:
+*list*
+```python
+[
+    [
+        short_name: am-letzte-maskebal, 
+        drama_type: comedy, 
+        anger: 0.5, 
+        anticipation: 0.5, 
+        ...:...
+    ]
+]
+```
+graphic output: barplot
+
+```shell
+python3 graphic.py --shortName zwei-erfindunge
+python3 graphic.py --emotion joy
+python3 graphic.py --emotion joy comedy
+python3 graphic.py horreur
+python3 graphic.py
+```
+sample image:
+![sample image](../ed_analyse_pieces/sample_group.png)
